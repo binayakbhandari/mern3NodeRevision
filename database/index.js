955a7385e9eae8ff2698d9ef6103de8ab9cd0513
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-25 23:51:12 +0545

    push

--- database/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-async function connectDatabase(){
-    await mongoose.connect(process.env.MONGODB_URL)
-    console.log("Database connected successfully")
-}
-
-module.exports = connectDatabase
