46161c770093916508848a8c685c1f72636a4e3c
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-28 19:11:47 +0545

    updated cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const cors = require('cors')
 
 app.use(cors(
     {
-        origin : ["http://localhost:5173","https://mern-3-0-project1-react.vercel.app"]
+        origin : "https://mern-3-0-project1-react.vercel.app"
     }
 ))
 
