0ee48ecdbca016a0983959f2c6a0b6323e4c6d3a
binayakbhandari <binayakbhandari60@gmail.com>
2024-09-28 18:51:17 +0545

    error solved

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const cors = require('cors')
 
 app.use(cors(
     {
-        origin : "http://localhost:5173"
+        origin : ["http://localhost:5173", "https://mern-3-0-project1-react.vercel.app/"]
     }
 ))
 
